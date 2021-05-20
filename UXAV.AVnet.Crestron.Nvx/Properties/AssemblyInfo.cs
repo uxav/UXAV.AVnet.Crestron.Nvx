59f5d8c8913c1cb07fbab760bce2c7e8373df149
Mike Jobson <mike@ux.digital>
2021-05-20 06:19:19 +0100

    Changed AVnet

--- UXAV.AVnet.Crestron.Nvx/Properties/AssemblyInfo.cs
+++ UXAV.AVnet.Crestron.Nvx/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UXAV.CrestronNvx")]
-[assembly: AssemblyDescription("Device control for a device")]
+[assembly: AssemblyTitle("UXAV.AVnet.Crestron.Nvx")]
+[assembly: AssemblyDescription("AVnet wrapper for Crestron NVX control")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("UXAV")]
-[assembly: AssemblyProduct("UXAV.CrestronNvx")]
-[assembly: AssemblyCopyright("Copyright © UXAV 2020")]
+[assembly: AssemblyProduct("UXAV.AVnet.Crestron.Nvx")]
+[assembly: AssemblyCopyright("Copyright © UXAV 2021")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -19,6 +19,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3F50C7F6-51ED-4B02-8FD1-A6A72B39A9A3")]
 
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
